25ab60c244b90674a84abc30bf653c8ee932804b
SupDex <symeton@gmail.com>
2022-05-08 22:10:53 +0300

    require primary_key file

--- lib/graphql/connections.rb
+++ lib/graphql/connections.rb
@@ -17,4 +17,8 @@ require "graphql/connections/keyset/base"
 require "graphql/connections/keyset/asc"
 require "graphql/connections/keyset/desc"
 
+require "graphql/connections/primary_key/base"
+require "graphql/connections/primary_key/asc"
+require "graphql/connections/primary_key/desc"
+
 require "graphql/connections/chewy"
